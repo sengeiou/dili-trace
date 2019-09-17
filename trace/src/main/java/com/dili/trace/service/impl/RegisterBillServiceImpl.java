5f123364f594471aed5003290b4c15ef3beff151
wangguofeng <wangguofeng@diligrp.com>
2019-09-17 17:02:03 +0800

    有检测报告的登记单,审核通过时不再直接进入已检测状态

--- trace/src/main/java/com/dili/trace/service/impl/RegisterBillServiceImpl.java
+++ trace/src/main/java/com/dili/trace/service/impl/RegisterBillServiceImpl.java
@@ -224,12 +224,12 @@ public class RegisterBillServiceImpl extends BaseServiceImpl<RegisterBill, Long>
 			registerBill.setOperatorId(userTicket.getId());
 			if (pass) {
 				registerBill.setState(RegisterBillStateEnum.WAIT_SAMPLE.getCode().intValue());
-				if (StringUtils.isNotBlank(registerBill.getDetectReportUrl())) {
-					// 有检测报告，直接通过检测
-//					registerBill.setLatestDetectTime(new Date());
-					registerBill.setState(RegisterBillStateEnum.ALREADY_CHECK.getCode());
-					registerBill.setDetectState(BillDetectStateEnum.PASS.getCode());
-				}
+//				if (StringUtils.isNotBlank(registerBill.getDetectReportUrl())) {
+//					// 有检测报告，直接通过检测
+////					registerBill.setLatestDetectTime(new Date());
+//					registerBill.setState(RegisterBillStateEnum.ALREADY_CHECK.getCode());
+//					registerBill.setDetectState(BillDetectStateEnum.PASS.getCode());
+//				}
 			} else {
 				registerBill.setState(-1);
 			}
