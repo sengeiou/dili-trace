72e18321c9b3444511712d9418b071c158e7b159
jiangchengyong <jiangchengyong@diligrp.com>
2019-10-11 10:10:27 +0800

    新增用户理货区表

---
+++ trace/src/main/java/com/dili/trace/dao/UserTailyAreaMapper.java
@@ -0,0 +1,7 @@
+package com.dili.trace.dao;
+
+import com.dili.ss.base.MyMapper;
+import com.dili.trace.domain.UserTailyArea;
+
+public interface UserTailyAreaMapper extends MyMapper<UserTailyArea> {
+}
---
+++ trace/src/main/java/com/dili/trace/service/UserTailyAreaService.java
@@ -0,0 +1,11 @@
+package com.dili.trace.service;
+
+import com.dili.ss.base.BaseService;
+import com.dili.trace.domain.UserTailyArea;
+
+/**
+ * 由MyBatis Generator工具自动生成
+ * This file was generated on 2019-10-11 10:05:11.
+ */
+public interface UserTailyAreaService extends BaseService<UserTailyArea, Long> {
+}
