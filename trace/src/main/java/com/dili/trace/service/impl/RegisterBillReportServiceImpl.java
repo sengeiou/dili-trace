014273dbecc2eeb572562dae38d1a792109fed31
wangguofeng <wangguofeng@diligrp.com>
2020-03-19 17:58:18 +0800

    修复排序错误

--- trace/src/main/java/com/dili/trace/service/impl/RegisterBillReportServiceImpl.java
+++ trace/src/main/java/com/dili/trace/service/impl/RegisterBillReportServiceImpl.java
@@ -2,7 +2,13 @@ package com.dili.trace.service.impl;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import org.apache.commons.lang3.RegExUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,13 +23,7 @@ import com.dili.trace.service.RegisterBillReportService;
 public class RegisterBillReportServiceImpl implements RegisterBillReportService{
 	@Autowired RegisterBillMapper mapper;
 	public EasyuiPageOutput listPageGroupByProduct(RegisterBillReportQueryDto dto) throws Exception {
-		if(dto.getPage()==null||dto.getPage()<=0) {
-			dto.setPage(1);
-		}
-		if(dto.getRows()==null||dto.getRows()<=0) {
-			dto.setRows(10);
-		}
-		dto.setOffSet((dto.getPage()-1)*dto.getRows());
+		dto=this.checkAndSetPageParams(dto);
 		dto.setGroupByColumns(String.join(",",Arrays.asList("product_name")));
 		Long total = this.mapper.listPageGroupByProductCount(dto);
 		List<GroupByProductReportDto>list=this.mapper.listPageGroupByProduct(dto);
@@ -39,13 +39,7 @@ public class RegisterBillReportServiceImpl implements RegisterBillReportService{
 	
 	@Override
 	public EasyuiPageOutput listPageGroupByNameAndPlate(RegisterBillReportQueryDto dto) throws Exception {
-		if(dto.getPage()==null||dto.getPage()<=0) {
-			dto.setPage(1);
-		}
-		if(dto.getRows()==null||dto.getRows()<=0) {
-			dto.setRows(10);
-		}
-		dto.setOffSet((dto.getPage()-1)*dto.getRows());
+		dto=this.checkAndSetPageParams(dto);
 		dto.setGroupByColumns(String.join(",",Arrays.asList("name,plate")));
 		Long total = this.mapper.listPageGroupByProductCount(dto);
 		List<GroupByProductReportDto>list=this.mapper.listPageGroupByProduct(dto);
@@ -60,13 +54,7 @@ public class RegisterBillReportServiceImpl implements RegisterBillReportService{
 	}
 	@Override
 	public EasyuiPageOutput listPageGroupByOrigin(RegisterBillReportQueryDto dto) throws Exception {
-		if(dto.getPage()==null||dto.getPage()<=0) {
-			dto.setPage(1);
-		}
-		if(dto.getRows()==null||dto.getRows()<=0) {
-			dto.setRows(10);
-		}
-		dto.setOffSet((dto.getPage()-1)*dto.getRows());
+		dto=this.checkAndSetPageParams(dto);
 		dto.setGroupByColumns(String.join(",",Arrays.asList("origin_name")));
 		Long total = this.mapper.listPageGroupByProductCount(dto);
 		List<GroupByProductReportDto>list=this.mapper.listPageGroupByProduct(dto);
@@ -78,6 +66,52 @@ public class RegisterBillReportServiceImpl implements RegisterBillReportService{
 		dto.setGroupByColumns(String.join(",",Arrays.asList("origin_name")));
 		List<GroupByProductReportDto>list=this.mapper.listPageGroupByProduct(dto);
 		return list;
+	}
+	private RegisterBillReportQueryDto checkAndSetPageParams(RegisterBillReportQueryDto dto) {
+		
+		if(dto.getPage()==null||dto.getPage()<=0) {
+			dto.setPage(1);
+		}
+		if(dto.getRows()==null||dto.getRows()<=0) {
+			dto.setRows(10);
+		}
+		dto.setOffSet((dto.getPage()-1)*dto.getRows());
+		if(StringUtils.isNotBlank(dto.getSort())) {
+			String sort=Stream.of(dto.getSort().split(",")).filter(StringUtils::isNotBlank).map(String::trim).map(str->{
+				StringBuffer sb = new StringBuffer();
+				Pattern p = Pattern.compile("w*[A-Z]w*");
+				Matcher m = p.matcher(str);
+				while (m.find())
+				{ // Find each match in turn; String can't do this.
+				//String name = m.group(1); // Access a submatch group; String can't do this.
+				m.appendReplacement(sb,"_"+m.group().toLowerCase());
+				System.out.println("m.group() is= " + m.group());
+				}
+				m.appendTail(sb);
+				return sb.toString();
+				
+			}).collect(Collectors.joining(","));
+			dto.setSort(sort);
+		}
+		return dto;
+		
+		
+		
+	}
+	public static void main(String[] args) {
+		String in = "aBcABDl";
+		System.out.println("in is= " + in);
+		StringBuffer sb = new StringBuffer();
+		Pattern p = Pattern.compile("w*[A-Z]w*");
+		Matcher m = p.matcher(in);
+		while (m.find())
+		{ // Find each match in turn; String can't do this.
+		//String name = m.group(1); // Access a submatch group; String can't do this.
+		m.appendReplacement(sb,"_"+m.group().toLowerCase());
+		System.out.println("m.group() is= " + m.group());
+		}
+		m.appendTail(sb);
+		System.out.println("sb is= " + sb);
 	}
 
 
