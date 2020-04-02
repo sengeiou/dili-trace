30151fae07dd8a232232e7c1ca7322bfab4efddf
wangguofeng <wangguofeng@diligrp.com>
2020-04-02 15:38:25 +0800

    修改sql输出配置

--- trace/src/main/java/com/dili/common/config/ProxyDataSourceConfig.java
+++ trace/src/main/java/com/dili/common/config/ProxyDataSourceConfig.java
@@ -1,15 +1,11 @@
 package com.dili.common.config;
 
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
-
-import com.github.gavlyukovskiy.boot.jdbc.decorator.dsproxy.ProxyDataSourceBuilderConfigurer;
 
 import net.ttddyy.dsproxy.support.ProxyDataSource;
 
@@ -18,7 +14,7 @@ public class ProxyDataSourceConfig {
 
 	@Bean
 	@Primary
-	ProxyDataSource proxyDatasource(@Autowired DataSource ds) {
+	ProxyDataSource proxyDatasource(@Autowired AbstractRoutingDataSource ds) {
 
 		return new ProxyDataSource(ds);
 	}
