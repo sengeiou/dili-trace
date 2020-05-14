7e265c49d4e4a29c9e91778213e9c9440be70774
wangguofeng <wangguofeng@diligrp.com>
2020-05-14 17:17:45 +0800

    增加信息判断

--- trace/src/main/java/com/dili/trace/service/UserQrItemDetailService.java
+++ trace/src/main/java/com/dili/trace/service/UserQrItemDetailService.java
@@ -1,6 +1,7 @@
 package com.dili.trace.service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.dili.common.exception.BusinessException;
 import com.dili.ss.base.BaseServiceImpl;
@@ -26,13 +27,49 @@ public class UserQrItemDetailService extends BaseServiceImpl<UserQrItemDetail, L
     UserQrItemService userQrItemService;
     @Autowired
     UpStreamService upStreamService;
+    @Autowired
+    RegisterBillService registerBillService;
 
     /**
      * 通过登记单来更新二维码状态
      */
     public void updateQrItemDetail(RegisterBill registerBill) {
-        UserQrItem queryCondition = new UserQrItem();
-        queryCondition.setQrItemType(QrItemTypeEnum.BILL.getCode());
+
+        User userItem = this.userService.get(registerBill.getUserId());
+        if (userItem == null) {
+            throw new BusinessException("未能查询到用户信息");
+        }
+        // 查询并添加UserQrItem
+        UserQrItem qrItem = new UserQrItem();
+        qrItem.setUserId(userItem.getId());
+        qrItem.setQrItemType(QrItemTypeEnum.UPSTREAM.getCode());
+        List<UserQrItem> qrItemList = this.userQrItemService.listByExample(qrItem);
+        if (qrItemList.isEmpty()) {
+            qrItem.setQrItemStatus(QrItemStatusEnum.GREEN.getCode());
+            this.userQrItemService.insertSelective(qrItem);
+        } else {
+            qrItem = qrItemList.stream().findFirst().orElse(null);
+        }
+        // 查询并添加UserQrItemDetail
+        UserQrItemDetail qrItemDetail = new UserQrItemDetail();
+        qrItemDetail.setObjectId(String.valueOf(registerBill.getId()));
+        qrItemDetail.setUserQrItemId(qrItem.getId());
+        if (this.listByExample(qrItemDetail).stream().count() == 0) {
+            this.insertSelective(qrItemDetail);
+        }
+        // 查询所有UserQrItemDetail并进行状态判断
+        UserQrItemDetail query = new UserQrItemDetail();
+        query.setUserQrItemId(qrItem.getId());
+        List<RegisterBill> billList = this.listByExample(query).stream()
+                .map(detail -> Long.parseLong(detail.getObjectId())).map(billId -> this.registerBillService.get(billId))
+                .collect(Collectors.toList());
+        boolean withoutUrl = billList.stream().anyMatch(bill -> {
+            return StringUtils.isAllBlank(registerBill.getOriginCertifiyUrl(), registerBill.getDetectReportUrl());
+        });
+        if (withoutUrl) {
+            qrItem.setQrItemStatus(QrItemStatusEnum.YELLOW.getCode());
+            this.userQrItemService.updateSelective(qrItem);
+        }
 
     }
 
@@ -48,15 +85,41 @@ public class UserQrItemDetailService extends BaseServiceImpl<UserQrItemDetail, L
         if (upStreamItem == null) {
             throw new BusinessException("未能查询到上游信息");
         }
-        QrItemStatusEnum itemStatus = QrItemStatusEnum.GREEN;
-        if (UpStreamTypeEnum.CORPORATE.getCode().equals(upStreamItem.getUpstreamType())) {
-            if (StringUtils.isAnyBlank(upStreamItem.getName(), upStreamItem.getLicense(), upStreamItem.getTelphone(),
-            upStreamItem.get.getLicenseUrl(), userItem.getCardNo(), userItem.getLegalPerson(), userItem.getPhone())
-            || userItem.getMarketId() == null) {
-        itemStatus = QrItemStatusEnum.YELLOW;
-    }
-        } else {
 
+        UserQrItem qrItem = new UserQrItem();
+        qrItem.setUserId(userItem.getId());
+        qrItem.setQrItemType(QrItemTypeEnum.UPSTREAM.getCode());
+        List<UserQrItem> qrItemList = this.userQrItemService.listByExample(qrItem);
+        if (qrItemList.isEmpty()) {
+            qrItem.setQrItemStatus(QrItemStatusEnum.GREEN.getCode());
+            this.userQrItemService.insertSelective(qrItem);
+        } else {
+            qrItem = qrItemList.stream().findFirst().orElse(null);
+        }
+
+        // 查询所有UserQrItemDetail并进行状态判断
+        UserQrItemDetail query = new UserQrItemDetail();
+        query.setUserQrItemId(qrItem.getId());
+        List<UpStream> upStreamList = this.listByExample(query).stream()
+                .map(detail -> Long.parseLong(detail.getObjectId()))
+                .map(upStreamId -> this.upStreamService.get(upStreamId)).collect(Collectors.toList());
+        boolean withoutAllInfo = upStreamList.stream().anyMatch(item -> {
+            if (UpStreamTypeEnum.CORPORATE.getCode().equals(item.getUpstreamType())) {
+                if (StringUtils.isAnyBlank(item.getName(), item.getBusinessLicenseUrl(), item.getLicense(),
+                        item.getLicenseUrl(), item.getIdCard(), item.getLegalPerson(), item.getTelphone())) {
+                    return true;
+                }
+            } else {
+                if (StringUtils.isAnyBlank(item.getName(), item.getCardNoFrontUrl(), item.getCardNoBackUrl(),
+                        item.getIdCard(), item.getTelphone())) {
+                    return true;
+                }
+            }
+            return false;
+        });
+        if (withoutAllInfo) {
+            qrItem.setQrItemStatus(QrItemStatusEnum.YELLOW.getCode());
+            this.userQrItemService.updateSelective(qrItem);
         }
 
     }
@@ -70,37 +133,48 @@ public class UserQrItemDetailService extends BaseServiceImpl<UserQrItemDetail, L
         if (userItem == null) {
             throw new BusinessException("未能查询到用户信息");
         }
-        QrItemStatusEnum itemStatus = QrItemStatusEnum.GREEN;
-        if (UserTypeEnum.CORPORATE.getCode().equals(userItem.getUserType())) {
-            if (StringUtils.isAnyBlank(userItem.getName(), userItem.getBusinessLicenseUrl(), userItem.getLicense(),
-                    userItem.getLicenseUrl(), userItem.getCardNo(), userItem.getLegalPerson(), userItem.getPhone())
-                    || userItem.getMarketId() == null) {
-                itemStatus = QrItemStatusEnum.YELLOW;
-            }
-        } else {
-            if (StringUtils.isAnyBlank(userItem.getName(), userItem.getCardNoFrontUrl(), userItem.getCardNoBackUrl(),
-                    userItem.getCardNo(), userItem.getPhone()) || userItem.getMarketId() == null) {
-                itemStatus = QrItemStatusEnum.YELLOW;
-            }
-        }
         UserQrItem qrItem = new UserQrItem();
-        qrItem.setUserId(userId);
+        qrItem.setUserId(userItem.getId());
         qrItem.setQrItemType(QrItemTypeEnum.USER.getCode());
         List<UserQrItem> qrItemList = this.userQrItemService.listByExample(qrItem);
         if (qrItemList.isEmpty()) {
+            qrItem.setQrItemStatus(QrItemStatusEnum.GREEN.getCode());
             this.userQrItemService.insertSelective(qrItem);
         } else {
             qrItem = qrItemList.stream().findFirst().orElse(null);
         }
-        if (!itemStatus.getCode().equals(qrItem.getQrItemStatus())) {
-            qrItem.setQrItemStatus(itemStatus.getCode());
-            this.userQrItemService.updateSelective(qrItem);
-        }
+
         UserQrItemDetail qrItemDetail = new UserQrItemDetail();
         qrItemDetail.setObjectId(String.valueOf(userId));
         qrItemDetail.setUserQrItemId(qrItem.getId());
         if (this.listByExample(qrItemDetail).stream().count() == 0) {
             this.insertSelective(qrItemDetail);
+        }
+
+        // 查询所有UserQrItemDetail并进行状态判断
+        UserQrItemDetail query = new UserQrItemDetail();
+        query.setUserQrItemId(qrItem.getId());
+        List<User> upStreamList = this.listByExample(query).stream().map(detail -> Long.parseLong(detail.getObjectId()))
+                .map(upStreamId -> this.userService.get(upStreamId)).collect(Collectors.toList());
+        boolean withoutAllInfo = upStreamList.stream().anyMatch(item -> {
+            if (UserTypeEnum.CORPORATE.getCode().equals(item.getUserType())) {
+                if (StringUtils.isAnyBlank(item.getName(), item.getBusinessLicenseUrl(), item.getLicense(),
+                        item.getLicenseUrl(), item.getCardNo(), item.getLegalPerson(), item.getPhone())
+                        || item.getMarketId() == null) {
+                    return true;
+                }
+            } else {
+                if (StringUtils.isAnyBlank(item.getName(), item.getCardNoFrontUrl(),
+                        item.getCardNoBackUrl(), item.getCardNo(), item.getPhone())
+                        || item.getMarketId() == null) {
+                    return true;
+                }
+            }
+            return false;
+        });
+        if (withoutAllInfo) {
+            qrItem.setQrItemStatus(QrItemStatusEnum.YELLOW.getCode());
+            this.userQrItemService.updateSelective(qrItem);
         }
 
     }
