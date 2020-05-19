b6f51bc8a97b6a0b5e7c4e84b538bed54c8a7956
wangguofeng <wangguofeng@diligrp.com>
2020-05-19 09:37:08 +0800

    增加mapper

---
+++ trace/src/main/java/com/dili/trace/dao/CheckinRecordMapper.java
@@ -0,0 +1,8 @@
+package com.dili.trace.dao;
+
+import com.dili.ss.base.MyMapper;
+import com.dili.trace.domain.CheckinRecord;
+
+public interface CheckinRecordMapper extends MyMapper<CheckinRecord> {
+    
+}
