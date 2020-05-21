2f43bfab6c0140611bcd0ce1cffaab3854c9739e
wangguofeng <wangguofeng@diligrp.com>
2020-05-21 14:05:25 +0800

    修复接口错误

--- trace/src/main/java/com/dili/trace/api/CheckinRecordApi.java
+++ trace/src/main/java/com/dili/trace/api/CheckinRecordApi.java
@@ -1,5 +1,7 @@
 package com.dili.trace.api;
 
+import java.util.Optional;
+
 import javax.annotation.Resource;
 
 import org.slf4j.Logger;
@@ -21,6 +23,7 @@ import com.dili.trace.api.dto.CheckInApiListOutput;
 import com.dili.trace.domain.CheckinRecord;
 import com.dili.trace.domain.RegisterBill;
 import com.dili.trace.domain.User;
+import com.dili.trace.dto.OperatorUser;
 import com.dili.trace.dto.RegisterBillDto;
 import com.dili.trace.service.CheckinRecordService;
 import com.dili.trace.service.RegisterBillService;
@@ -33,63 +36,68 @@ import io.swagger.annotations.Api;
 @InterceptConfiguration
 @RequestMapping(value = "/api/checkinRecordApi")
 public class CheckinRecordApi {
-    private static final Logger logger = LoggerFactory.getLogger(UserApi.class);
-    @Resource
-    private UserService userService;
-    @Resource
-    private SessionContext sessionContext;
-    @Autowired
-    RegisterBillService registerBillService;
-    @Autowired
-    CheckinRecordService checkinRecordService;
+	private static final Logger logger = LoggerFactory.getLogger(UserApi.class);
+	@Resource
+	private UserService userService;
+	@Resource
+	private SessionContext sessionContext;
+	@Autowired
+	RegisterBillService registerBillService;
+	@Autowired
+	CheckinRecordService checkinRecordService;
 
-    /**
-     * 分页查询需要被进场查询的信息
-     */
-    @RequestMapping(value = "/listPagedAvailableCheckInData.api", method = { RequestMethod.POST, RequestMethod.GET })
-    public BaseOutput<BasePage<RegisterBill>> listPagedAvailableCheckInData(@RequestBody RegisterBillDto query) {
-        User user = userService.get(sessionContext.getAccountId());
-        if (user == null) {
-            return BaseOutput.failure("未登陆用户");
-        }
-        BasePage<CheckInApiListOutput> page = this.checkinRecordService.listCheckInApiListOutputPage(query);
+	/**
+	 * 分页查询需要被进场查询的信息
+	 */
+	@RequestMapping(value = "/listPagedAvailableCheckInData.api", method = { RequestMethod.POST, RequestMethod.GET })
+	public BaseOutput<BasePage<RegisterBill>> listPagedAvailableCheckInData(@RequestBody RegisterBillDto query) {
+//        User user = userService.get(sessionContext.getAccountId());
+//        if (user == null) {
+//            return BaseOutput.failure("未登陆用户");
+//        }
+		BasePage<CheckInApiListOutput> page = this.checkinRecordService.listCheckInApiListOutputPage(query);
 
-        return BaseOutput.success().setData(page);
-    }
-    /**
-     * 分页查询需要被进场查询的信息
-     */
-    @RequestMapping(value = "/getCheckInDetail.api", method = { RequestMethod.POST, RequestMethod.GET })
-    public BaseOutput<Object> getCheckInDetail(@RequestBody RegisterBillDto query) {
-        User user = userService.get(sessionContext.getAccountId());
-        if (user == null) {
-            return BaseOutput.failure("未登陆用户");
-        }
-        if(query==null||query.getId()==null) {
-        	  return BaseOutput.failure("参数错误");
-        }
-        CheckInApiDetailOutput detail=this.checkinRecordService.getCheckInDetail(query.getId());
-        
-        return BaseOutput.success().setData(detail);
-    }
-    /**
-     * 进场
-     */
-    @RequestMapping(value = "/doCheckin.api", method = { RequestMethod.POST, RequestMethod.GET })
-    public BaseOutput<Long> doCheckin(@RequestBody CheckInApiInput input) {
-        User user = userService.get(sessionContext.getAccountId());
-        if (user == null) {
-            return BaseOutput.failure("未登陆用户");
-        }
-        try {
-            CheckinRecord checkinRecord=    this.checkinRecordService.doCheckin(user, input);
-            return BaseOutput.success().setData(checkinRecord.getId());
-        } catch (BusinessException e) {
+		return BaseOutput.success().setData(page);
+	}
 
-            return BaseOutput.failure(e.getMessage());
-        } catch (Exception e) {
-            logger.error(e.getMessage(), e);
-            return BaseOutput.failure("服务端出错");
-        }
-    }
+	/**
+	 * 分页查询需要被进场查询的信息
+	 */
+	@RequestMapping(value = "/getCheckInDetail.api", method = { RequestMethod.POST, RequestMethod.GET })
+	public BaseOutput<CheckInApiDetailOutput> getCheckInDetail(@RequestBody RegisterBillDto query) {
+//		User user = userService.get(sessionContext.getAccountId());
+//		if (user == null) {
+//			return BaseOutput.failure("未登陆用户");
+//		}
+		if (query == null || query.getId() == null) {
+			return BaseOutput.failure("参数错误");
+		}
+		CheckInApiDetailOutput detail = this.checkinRecordService.getCheckInDetail(query.getId()).orElse(null);
+		if (detail != null) {
+			return BaseOutput.success().setData(detail);
+		}
+		return BaseOutput.failure("没有查找到详情");
+
+	}
+
+	/**
+	 * 进场
+	 */
+	@RequestMapping(value = "/doCheckin.api", method = { RequestMethod.POST, RequestMethod.GET })
+	public BaseOutput<Long> doCheckin(@RequestBody CheckInApiInput input) {
+//		User user = userService.get(sessionContext.getAccountId());
+//		if (user == null) {
+//			return BaseOutput.failure("未登陆用户");
+//		}
+		try {
+			CheckinRecord checkinRecord = this.checkinRecordService.doCheckin(new OperatorUser(0L, "test"), input);
+			return BaseOutput.success().setData(checkinRecord.getId());
+		} catch (BusinessException e) {
+
+			return BaseOutput.failure(e.getMessage());
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+			return BaseOutput.failure("服务端出错");
+		}
+	}
 }
--- trace/src/main/java/com/dili/trace/service/CheckinRecordService.java
+++ trace/src/main/java/com/dili/trace/service/CheckinRecordService.java
@@ -3,6 +3,7 @@ package com.dili.trace.service;
 import java.util.Date;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,87 +20,113 @@ import com.dili.trace.api.dto.CheckInApiListOutput;
 import com.dili.trace.dao.CheckinRecordMapper;
 import com.dili.trace.domain.CheckinRecord;
 import com.dili.trace.domain.RegisterBill;
+import com.dili.trace.domain.UpStream;
 import com.dili.trace.domain.User;
+import com.dili.trace.dto.OperatorUser;
 import com.dili.trace.glossary.CheckinStatusEnum;
 import com.dili.trace.glossary.RegisterBillStateEnum;
 
 @Service
 public class CheckinRecordService extends BaseServiceImpl<CheckinRecord, Long> {
-    @Autowired
-    UserService userService;
-    @Autowired
-    RegisterBillService registerBillService;
-    @Autowired
-    SeparateSalesRecordService separateSalesRecordService;
-    @Autowired
-    CheckinRecordMapper checkinRecordMapper;
+	@Autowired
+	UserService userService;
+	@Autowired
+	RegisterBillService registerBillService;
+	@Autowired
+	SeparateSalesRecordService separateSalesRecordService;
+	@Autowired
+	CheckinRecordMapper checkinRecordMapper;
+	@Autowired
+	UpStreamService upStreamService;
 
-    @Transactional
-    public CheckinRecord doCheckin(User operateUser, CheckInApiInput checkInApiInput) {
-        if (checkInApiInput == null || checkInApiInput.getBillIdList() == null
-                || checkInApiInput.getCheckinStatus() == null) {
-            throw new BusinessException("参数错误");
-        }
-        CheckinStatusEnum checkinStatusEnum = CheckinStatusEnum.fromCode(checkInApiInput.getCheckinStatus());
+	@Transactional
+	public CheckinRecord doCheckin(OperatorUser operateUser, CheckInApiInput checkInApiInput) {
+		if (checkInApiInput == null || checkInApiInput.getBillIdList() == null
+				|| checkInApiInput.getCheckinStatus() == null) {
+			throw new BusinessException("参数错误");
+		}
+		CheckinStatusEnum checkinStatusEnum = CheckinStatusEnum.fromCode(checkInApiInput.getCheckinStatus());
 
-        if (checkinStatusEnum == null) {
-            throw new BusinessException("参数错误");
-        }
-        List<RegisterBill> billList = checkInApiInput.getBillIdList().stream().map(billId -> {
+		if (checkinStatusEnum == null) {
+			throw new BusinessException("参数错误");
+		}
+		List<RegisterBill> billList = checkInApiInput.getBillIdList().stream().map(billId -> {
 
-            RegisterBill bill = this.registerBillService.get(billId);
-            if (bill == null) {
-                return null;
-            } else {
-                if (RegisterBillStateEnum.WAIT_AUDIT.getCode().equals(bill.getState())) {
-                    return bill;
-                }
-            }
+			RegisterBill bill = this.registerBillService.get(billId);
+			if (bill == null) {
+				return null;
+			} else {
+				if (RegisterBillStateEnum.WAIT_AUDIT.getCode().equals(bill.getState())) {
+					return bill;
+				}
+			}
 
-            return null;
-        }).filter(Objects::nonNull).collect(Collectors.toList());
-        if (billList.isEmpty()) {
-            throw new BusinessException("没有可以进场的登记单");
-        }
+			return null;
+		}).filter(Objects::nonNull).collect(Collectors.toList());
+		if (billList.isEmpty()) {
+			throw new BusinessException("没有可以进场的登记单");
+		}
 
-        // User user = this.userService.get(checkInApiInput.getUserId());
-        // if (user == null) {
-        // throw new BusinessException("数据错误");
-        // }
+		// User user = this.userService.get(checkInApiInput.getUserId());
+		// if (user == null) {
+		// throw new BusinessException("数据错误");
+		// }
 
-        CheckinRecord checkinRecord = new CheckinRecord();
-        checkinRecord.setCheckinStatus(checkinStatusEnum.getCode());
-        checkinRecord.setOperatorId(operateUser.getId());
-        checkinRecord.setOperatorName(operateUser.getName());
-        checkinRecord.setRemark(checkInApiInput.getRemark());
-        checkinRecord.setCreated(new Date());
-        checkinRecord.setModified(new Date());
-        int returnValue=this.insertSelective(checkinRecord);
-        billList.stream().forEach(bill -> {
-            this.separateSalesRecordService.checkInSeparateSalesRecord(checkinRecord.getId(), bill.getId());
-            RegisterBill updatable = DTOUtils.newDTO(RegisterBill.class);
-            updatable.setId(bill.getId());
-            updatable.setState(RegisterBillStateEnum.WAIT_CHECK.getCode());
-            this.registerBillService.updateSelective(updatable);
-        });
+		CheckinRecord checkinRecord = new CheckinRecord();
+		checkinRecord.setCheckinStatus(checkinStatusEnum.getCode());
+		checkinRecord.setOperatorId(operateUser.getId());
+		checkinRecord.setOperatorName(operateUser.getName());
+		checkinRecord.setRemark(checkInApiInput.getRemark());
+		checkinRecord.setCreated(new Date());
+		checkinRecord.setModified(new Date());
+		int returnValue = this.insertSelective(checkinRecord);
+		billList.stream().forEach(bill -> {
+			this.separateSalesRecordService.checkInSeparateSalesRecord(checkinRecord.getId(), bill.getId());
+			RegisterBill updatable = DTOUtils.newDTO(RegisterBill.class);
+			updatable.setId(bill.getId());
+			updatable.setState(RegisterBillStateEnum.WAIT_CHECK.getCode());
+			this.registerBillService.updateSelective(updatable);
+		});
 
-        return checkinRecord;
+		return checkinRecord;
 
-    }
-    public CheckInApiDetailOutput getCheckInDetail(Long billId) {
-    	return null;
-    }
-    public BasePage<CheckInApiListOutput>listCheckInApiListOutputPage(RegisterBill query){
-    	Integer total=this.checkinRecordMapper.countlistCheckInRecord(query);
-    	List<CheckInApiListOutput> list = this.checkinRecordMapper.listCheckInRecord(query);
+	}
+
+	public Optional<CheckInApiDetailOutput> getCheckInDetail(Long billId) {
+		RegisterBill registerBill = this.registerBillService.get(billId);
+		if (registerBill != null) {
+			CheckInApiDetailOutput output = new CheckInApiDetailOutput();
+			Long upstreamId = registerBill.getUpStreamId();
+			Long userId = registerBill.getUserId();
+			if (upstreamId != null) {
+				UpStream upStream = this.upStreamService.get(upstreamId);
+				if (upStream != null) {
+					output.setUpstreamName(upStream.getName());
+					output.setUpstreamTelphone(upStream.getTelphone());
+				}
+			}
+			if (userId != null) {
+				User user = this.userService.get(userId);
+				if (user != null) {
+				}
+			}
+			return Optional.of(output);
+		}
+		return Optional.empty();
+	}
+
+	public BasePage<CheckInApiListOutput> listCheckInApiListOutputPage(RegisterBill query) {
+		query.setState(RegisterBillStateEnum.WAIT_CHECK.getCode());
+		Integer total = this.checkinRecordMapper.countlistCheckInRecord(query);
+		List<CheckInApiListOutput> list = this.checkinRecordMapper.listCheckInRecord(query);
 		BasePage<CheckInApiListOutput> result = new BasePage();
 
 		result.setPage(query.getPage());
 		result.setRows(query.getRows());
 		result.setTotalItem(total);
-		
+
 		return result;
-    	
-    }
+
+	}
 
 }
