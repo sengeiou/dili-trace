add88e1cd50f78445eb73a8aa75922d709e217bb
wangguofeng <wangguofeng@diligrp.com>
2020-05-26 13:06:43 +0800

    修复:查询条件错误

--- trace/src/test/java/com/dili/trace/service/SeparateSalesRecordServiceTest.java
+++ trace/src/test/java/com/dili/trace/service/SeparateSalesRecordServiceTest.java
@@ -12,9 +12,14 @@ public class SeparateSalesRecordServiceTest extends AutoWiredBaseTest {
 	SeparateSalesRecordService separateSalesRecordService;
 
 	@Test
-	public void dd() {
+	public void listByQueryInput() throws Exception {
 		SeparateSalesApiListQueryInput queryInput=new SeparateSalesApiListQueryInput();
 		queryInput.setUserId(3L);
+		queryInput.setId(2L);
+		queryInput.setCreatedStart("2020-12-12 12:12:12");
+		queryInput.setCreatedEnd("2020-12-12 23:23:23");
+		queryInput.setLikeProductName("abc");
+		this.separateSalesRecordService.listPageByQueryInput(queryInput);
 		SeparateSalesApiListOutput output=this.separateSalesRecordService.listByQueryInput(queryInput).stream().findFirst().orElse(null);
 		System.out.println(output);
 	}
