a24dbaa25fffa0c0de6197ad855d8aa8cdc61bd4
wangguofeng <wangguofeng@diligrp.com>
2020-05-28 19:04:51 +0800

    Revert "update ddl"
    This reverts commit 2d40d7f4f274ec12fbdb57e603fe9a1d8b060efb.

--- doc/ddl/hz/hz-20200528.sql
+++ doc/ddl/hz/hz-20200528.sql
@@ -1,3 +1,3 @@
-ALTER TABLE `checkinout_record` ADD COLUMN `user_name`  varchar(30)   NULL  COMMENT '业户名称';
-ALTER TABLE `checkinout_record` ADD COLUMN `product_name` varchar(20)   NULL COMMENT '商品名称';
-ALTER TABLE `checkinout_record` ADD COLUMN `sales_weight` int(11)  NULL COMMENT '商品重量';
+ALTER TABLE `checkinout_record` ADD COLUMN `user_name`  varchar(30)  NULL COMMENT '业户名称';
+ALTER TABLE `checkinout_record` ADD COLUMN `product_name` varchar(20) NOT NULL COMMENT '商品名称';
+ALTER TABLE `checkinout_record` ADD COLUMN `sales_weight` int(11) DEFAULT NULL COMMENT '商品重量';
