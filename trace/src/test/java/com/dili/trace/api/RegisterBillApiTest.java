2de08679cdf3da873a713ce87ca307f50d112961
wangguofeng <wangguofeng@diligrp.com>
2020-06-20 17:52:59 +0800

    调整testcase

--- trace/src/test/java/com/dili/trace/api/RegisterBillApiTest.java
+++ trace/src/test/java/com/dili/trace/api/RegisterBillApiTest.java
@@ -1,5 +1,8 @@
 package com.dili.trace.api;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -9,18 +12,23 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 
 import com.dili.common.entity.LoginSessionContext;
 import com.dili.ss.domain.BaseOutput;
+import com.dili.ss.dto.DTOUtils;
 import com.dili.trace.AutoWiredBaseTest;
 import com.dili.trace.domain.RegisterBill;
+import com.dili.trace.domain.User;
 import com.dili.trace.dto.CreateListBillParam;
 import com.dili.trace.dto.OperatorUser;
 import com.dili.trace.enums.BillVerifyStateEnum;
 import com.dili.trace.service.RegisterBillService;
+import com.dili.trace.service.UserService;
 
 public class RegisterBillApiTest extends AutoWiredBaseTest {
 	@Autowired
 	RegisterBillApi registerBillApi;
 	@Autowired
 	RegisterBillService registerBillService;
+	@Autowired
+	UserService userService;
 
 	@MockBean
 	LoginSessionContext sessionContext;
@@ -30,13 +38,31 @@ public class RegisterBillApiTest extends AutoWiredBaseTest {
 	@BeforeEach
 	public void before() {
 		MockitoAnnotations.initMocks(this);
+		User user = this.userService.listPageByExample(DTOUtils.newDTO(User.class)).getDatas().stream().findFirst()
+				.orElse(null);
+		Mockito.doReturn(user.getId()).when(sessionContext).getAccountId();
 	}
 
 	@Test
 	public void testcreateList() {
-		
-		CreateListBillParam createListBillParam=new CreateListBillParam();
-		this.registerBillApi.createList(createListBillParam);
+
+		RegisterBill query = new RegisterBill();
+		query.setVerifyState(BillVerifyStateEnum.NONE.getCode());
+		RegisterBill item = registerBillService.listPageByExample(query).getDatas().stream().findFirst().orElse(null);
+
+		CreateListBillParam createListBillParam = new CreateListBillParam();
+		List<RegisterBill> registerBills = new ArrayList<RegisterBill>();
+		createListBillParam.setRegisterBills(registerBills);
+
+		RegisterBill rb = new RegisterBill();
+		registerBills.add(rb);
+		rb.setWeight(10);
+		rb.setProductId(item.getProductId());
+		rb.setProductName(item.getProductName());
+		rb.setOriginId(item.getOriginId());
+		rb.setOriginName(item.getOriginName());
+		BaseOutput out = this.registerBillApi.createList(createListBillParam);
+		System.out.println(out.isSuccess());
 	}
 
 	@Test
