3e056bc58d0f3222981dfd62011932d8c3a7ab73
wangguofeng <wangguofeng@diligrp.com>
2020-07-13 18:16:49 +0800

    修改时去掉原因

--- trace/src/main/java/com/dili/trace/service/impl/RegisterBillServiceImpl.java
+++ trace/src/main/java/com/dili/trace/service/impl/RegisterBillServiceImpl.java
@@ -267,6 +267,7 @@ public class RegisterBillServiceImpl extends BaseServiceImpl<RegisterBill, Long>
 
 		input.setOperatorName(null);
 		input.setOperatorId(null);
+		input.setReason(null);
 		operatorUser.ifPresent(op->{
 			input.setOperatorName(op.getName());
 			input.setOperatorId(op.getId());
