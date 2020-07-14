469c6cb7506b16ce5bd01a8d1c8bedbf761ecb64
wangguofeng <wangguofeng@diligrp.com>
2020-07-14 18:50:45 +0800

    update ddl

--- doc/ddl/hz/hz-20200714.sql
+++ doc/ddl/hz/hz-20200714.sql
@@ -1,7 +1,7 @@
 
 alter table `batch_stock` rename to `product_stock`;
-alter table `trade_detail` change `batch_stock_id`  `product_stock_id` bigint(20) NOT NULL COMMENT '商品库存ID';
-alter table `trade_request` change `batch_stock_id`  `product_stock_id` bigint(20) NOT NULL COMMENT '商品库存ID';
+alter table `trade_detail` change `batch_stock_id`  `product_stock_id` bigint(20) NULL COMMENT '商品库存ID';
+alter table `trade_request` change `batch_stock_id`  `product_stock_id` bigint(20) NULL COMMENT '商品库存ID';
 
 ALTER TABLE `trade_detail` ADD `batch_no` varchar(20) NULL COMMENT '批次号';
 
