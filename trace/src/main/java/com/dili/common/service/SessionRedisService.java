31697b78e247fc43cc1314e93f5e9f49f0c01e6b
wangguofeng <wangguofeng@diligrp.com>
2020-07-16 18:03:57 +0800

    fix bug

--- trace/src/main/java/com/dili/common/service/SessionRedisService.java
+++ trace/src/main/java/com/dili/common/service/SessionRedisService.java
@@ -52,6 +52,10 @@ public class SessionRedisService {
         }
         SessionData sessionData = SessionData.fromMap(sessionMapData);
         logger.info("loadFromRedis:sessionData={}",sessionData.toMap());
+        if(StringUtils.isBlank(sessionData.getSessionId())){
+            sessionData.setSessionId(sessionId);
+        }
+        this.saveToRedis(sessionData);
 
         String accountRedisKey = this.getAccountRedisKey(sessionData);
         Map<Object, Object> accountMapData = (Map<Object, Object>) this.redisService.get(accountRedisKey);
@@ -61,8 +65,8 @@ public class SessionRedisService {
             String oldSessionKey=this.getSessionRedisKey(accountData.getSessionId());
             logger.info("del:session={}",oldSessionKey);
             this.redisService.del(oldSessionKey);
-            this.redisService.set(accountRedisKey, sessionData.toMap(), defaultConfiguration.getSessionExpire());
         }
+        this.redisService.set(accountRedisKey, sessionData.toMap(), defaultConfiguration.getSessionExpire());
         return Optional.of(sessionData);
     }
 
