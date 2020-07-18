2461c123debc20755eaf3fc99cce8db666fe2d7d
wangguofeng <wangguofeng@diligrp.com>
2020-07-18 13:50:44 +0800

    修改日志配置

--- trace/src/main/java/com/dili/common/interceptor/LoginInterceptor.java
+++ trace/src/main/java/com/dili/common/interceptor/LoginInterceptor.java
@@ -1,5 +1,12 @@
 package com.dili.common.interceptor;
 
+import java.io.Writer;
+import java.lang.annotation.Annotation;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import com.alibaba.fastjson.JSONObject;
 import com.dili.common.annotation.InterceptConfiguration;
 import com.dili.common.entity.ExecutionConstants;
@@ -13,68 +20,68 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.Writer;
-import java.lang.annotation.Annotation;
-
 /**
  * 
  * @author xuliang
  *
  */
 public class LoginInterceptor extends HandlerInterceptorAdapter {
-		private static final Logger logger=LoggerFactory.getLogger(LoginInterceptor.class);
-	    @Resource
-		private LoginSessionContext sessionContext;
-		@Autowired
-		UserAccessLogService userAccessLogService;
+	private static final Logger logger = LoggerFactory.getLogger(LoginInterceptor.class);
+	@Resource
+	private LoginSessionContext sessionContext;
+	@Autowired
+	UserAccessLogService userAccessLogService;
 
-		private void logRequest(HttpServletRequest request){
-			Integer loginType=this.sessionContext.getLoginType();
-			Long accountId=this.sessionContext.getAccountId();
-			String requestUri=request.getRequestURI();
-			logger.info("loginType={},accountId={},requestUri={}",loginType,accountId,requestUri);
+	private void logRequest(HttpServletRequest request) {
+		if (this.sessionContext.getSessionData() != null && this.sessionContext.getSessionData().getUserId() != null) {
+			String requestUri = request.getRequestURI();
+			logger.info("loginType={},accountId={},requestUri={}", this.sessionContext.getLoginType(),
+					this.sessionContext.getAccountId(), requestUri);
 			this.userAccessLogService.createUserAccessLog(this.sessionContext.getSessionData(), requestUri);
-		 
 		}
-	    @Override
-	    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-			
-			boolean value=this.preHandleCheck(request, response, handler);
-			if(value){
-				this.logRequest(request);
-			}
-			return value;
+	}
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+
+		boolean value = this.preHandleCheck(request, response, handler);
+		if (value) {
+			this.logRequest(request);
 		}
-		public boolean preHandleCheck(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-			
-	        if(!(handler instanceof HandlerMethod)){
-	        	return true;
-			}
-			InterceptConfiguration interceptConfiguration = findAnnotation((HandlerMethod) handler, InterceptConfiguration.class);
-			if(interceptConfiguration==null || !interceptConfiguration.loginRequired()){
-				return true;
-			}
-	        if(sessionContext.getAccountId()==null){//用户id为空表示未登陆
-				response.setContentType("application/json");
-				response.setCharacterEncoding("UTF-8");
-				BaseOutput baseOutput = new BaseOutput(ExecutionConstants.NO_LOGIN,"未登陆");
-                Writer writer=response.getWriter();
-                writer.write(JSONObject.toJSONString(baseOutput));
-                writer.flush();
-                if(writer!=null){
-                    writer.close();
-                }
-                return false;
-			}
+		return value;
+	}
+
+	public boolean preHandleCheck(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+
+		if (!(handler instanceof HandlerMethod)) {
 			return true;
 		}
+		InterceptConfiguration interceptConfiguration = findAnnotation((HandlerMethod) handler,
+				InterceptConfiguration.class);
+		if (interceptConfiguration == null || !interceptConfiguration.loginRequired()) {
+			return true;
+		}
+		if (sessionContext.getAccountId() == null) {// 用户id为空表示未登陆
+			response.setContentType("application/json");
+			response.setCharacterEncoding("UTF-8");
+			BaseOutput baseOutput = new BaseOutput(ExecutionConstants.NO_LOGIN, "未登陆");
+			Writer writer = response.getWriter();
+			writer.write(JSONObject.toJSONString(baseOutput));
+			writer.flush();
+			if (writer != null) {
+				writer.close();
+			}
+			return false;
+		}
+		return true;
+	}
 
-	    private <T extends Annotation> T findAnnotation(HandlerMethod handler, Class<T> annotationType) {
-	        T annotation = handler.getMethodAnnotation(annotationType);
-	        if (annotation != null) return annotation;
-	        return handler.getBeanType().getAnnotation(annotationType);
-	    }
+	private <T extends Annotation> T findAnnotation(HandlerMethod handler, Class<T> annotationType) {
+		T annotation = handler.getMethodAnnotation(annotationType);
+		if (annotation != null)
+			return annotation;
+		return handler.getBeanType().getAnnotation(annotationType);
+	}
 }
