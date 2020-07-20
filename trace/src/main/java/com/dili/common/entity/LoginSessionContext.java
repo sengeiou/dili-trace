5589648687a8d21b480a30fba537ca3e566d8944
wangguofeng <wangguofeng@diligrp.com>
2020-07-20 14:38:11 +0800

    fix bug

--- trace/src/main/java/com/dili/common/entity/LoginSessionContext.java
+++ trace/src/main/java/com/dili/common/entity/LoginSessionContext.java
@@ -28,8 +28,8 @@ public class LoginSessionContext implements Serializable {
 	}
 
 	public OperatorUser getLoginUserOrException(LoginIdentityTypeEnum identityType) {
-		if (this.getSessionData() == null || this.getSessionData().getIdentityType() == null
-				|| this.getSessionData().getUserId() != null) {
+		if (this.getSessionData() == null && this.getSessionData().getIdentityType() == null
+				&& this.getSessionData().getUserId() != null) {
 			return new OperatorUser(this.getSessionData().getUserId(), this.getSessionData().getUserName());
 		}
 		throw new TraceBusinessException("你还未登录");
