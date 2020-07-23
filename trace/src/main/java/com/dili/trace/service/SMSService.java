8e7915a6ccee3c45d0966838c49ea0bd5cc722d5
wangguofeng <wangguofeng@diligrp.com>
2020-07-23 14:24:34 +0800

    增加注释

--- trace/src/main/java/com/dili/trace/service/SMSService.java
+++ trace/src/main/java/com/dili/trace/service/SMSService.java
@@ -5,16 +5,15 @@ import com.dili.common.exception.TraceBusinessException;
 import com.dili.ss.domain.BaseOutput;
 import com.dili.ss.redis.service.RedisUtil;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public abstract class SMSService {
     @Autowired
     protected RedisUtil redisUtil;
-    // 验证码缓存前缀
+    /**注册验证码缓存前缀 */ 
     public final static String REDIS_SYSTEM_VERCODE_PREIX = "USER:VERIFICATION_CODE:";
 
-    // 重置密码验证码缓存前缀
+    /**重置密码验证码缓存前缀 */ 
     public final static String REDIS_SYSTEM_RENEW_PASSWORD_PREIX = "USER:RENEW_PASSWORD_CODE:";
 
     public abstract BaseOutput sendVerificationCodeMsg(JSONObject params, String phone, String verificationCode);
