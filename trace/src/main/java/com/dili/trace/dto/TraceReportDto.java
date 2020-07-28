1a10c3c86b2015768bc0707407417dda2d344018
wangguofeng <wangguofeng@diligrp.com>
2020-07-28 20:24:42 +0800

    百分比计算错误

--- trace/src/main/java/com/dili/trace/dto/TraceReportDto.java
+++ trace/src/main/java/com/dili/trace/dto/TraceReportDto.java
@@ -20,7 +20,7 @@ public class TraceReportDto {
         }
         BigDecimal uc=  new BigDecimal(this.userCount);
         if(BigDecimal.ZERO.compareTo(uc)!=0){
-          this.percentage=  new BigDecimal(this.billCount).add( new BigDecimal(this.tradeDetailBuyerCount)).divide(uc,2,RoundingMode.HALF_UP);
+          this.percentage=  new BigDecimal(this.billCount).add( new BigDecimal(this.tradeDetailBuyerCount)).multiply(BigDecimal.valueOf(100)).divide(uc,2,RoundingMode.HALF_UP);
         }
         return this.percentage;
     }
