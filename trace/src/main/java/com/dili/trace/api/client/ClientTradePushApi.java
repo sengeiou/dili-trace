e2c16bbbaed31ac57ada22652945114a9467b13a
asa.lee <asa@skywares.com>
2020-08-18 11:50:20 +0800

    可售库存与下架库存查询接口合并

--- trace/src/main/java/com/dili/trace/api/client/ClientTradePushApi.java
+++ trace/src/main/java/com/dili/trace/api/client/ClientTradePushApi.java
@@ -3,15 +3,14 @@ package com.dili.trace.api.client;
 import com.dili.common.annotation.InterceptConfiguration;
 import com.dili.common.exception.TraceBusinessException;
 import com.dili.ss.domain.BaseOutput;
-import com.dili.trace.domain.ImageCert;
-import com.dili.trace.domain.RegisterBill;
-import com.dili.trace.domain.TradePushLog;
-import com.dili.trace.domain.UpStream;
-import com.dili.trace.service.ImageCertService;
-import com.dili.trace.service.RegisterBillService;
-import com.dili.trace.service.TradePushService;
-import com.dili.trace.service.UpStreamService;
+import com.dili.ss.domain.BasePage;
+import com.dili.ss.dto.IDTO;
+import com.dili.trace.domain.*;
+import com.dili.trace.service.*;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,6 +22,8 @@ import java.util.List;
 @InterceptConfiguration
 @RequestMapping(value = "/api/client/clientTradePush")
 public class ClientTradePushApi {
+    private static final Logger logger = LoggerFactory.getLogger(ClientTradePushApi.class);
+
     @Autowired
     private RegisterBillService registerBillService;
 
@@ -35,11 +36,14 @@ public class ClientTradePushApi {
     @Autowired
     private TradePushService tradePushService;
 
+    @Autowired
+    TradeDetailService tradeDetailService;
+
     /**
      * 查询报备单
      */
     @SuppressWarnings("unchecked")
-    @RequestMapping(value = "/viewBillDetail.api", method = { RequestMethod.GET })
+    @RequestMapping(value = "/viewBillDetail.api", method = {RequestMethod.GET})
     public BaseOutput<RegisterBill> viewTradeDetail(@RequestParam Long billId) {
         try {
             RegisterBill registerBill = registerBillService.get(billId);
@@ -50,9 +54,9 @@ public class ClientTradePushApi {
             List<ImageCert> imageCerts = imageCertService.findImageCertListByBillId(billId);
             registerBill.setImageCerts(imageCerts);
             return BaseOutput.success().setData(registerBill);
-        }catch (TraceBusinessException e){
+        } catch (TraceBusinessException e) {
             return BaseOutput.failure(e.getMessage());
-        }catch (Exception e){
+        } catch (Exception e) {
             return BaseOutput.failure("查询数据出错");
         }
     }
@@ -61,16 +65,58 @@ public class ClientTradePushApi {
      * 查询报备单
      */
     @SuppressWarnings("unchecked")
-    @RequestMapping(value = "/push.api", method = { RequestMethod.POST })
+    @RequestMapping(value = "/push.api", method = {RequestMethod.POST})
     public BaseOutput doTradePush(@RequestBody TradePushLog pushLog) {
         try {
             tradePushService.tradePush(pushLog);
-        }catch (TraceBusinessException e){
+        } catch (TraceBusinessException e) {
             BaseOutput.failure(e.getMessage());
-        }catch (Exception e){
+        } catch (Exception e) {
             BaseOutput.failure("系统错误");
         }
 
         return BaseOutput.success();
+    }
+
+
+    @ApiOperation(value = "查询上架商品列表", notes = "查询上架商品列表")
+    @RequestMapping(value = "/shelvesProduct.api", method = RequestMethod.POST)
+    @InterceptConfiguration
+    public BaseOutput<BasePage<TradeDetail>> shelvesProduct(@RequestBody TradeDetail tradeDetail) {
+        if (tradeDetail == null || null == tradeDetail.getBuyerId()) {
+            return BaseOutput.failure("参数错误");
+        }
+        try {
+            tradeDetail.setSort("product_name");
+            tradeDetail.setOrder("desc");
+            tradeDetail.setMetadata(IDTO.AND_CONDITION_EXPR, " stock_weight > 0");
+            return BaseOutput.success().setData(tradeDetailService.listPageByExample(tradeDetail));
+        } catch (TraceBusinessException e) {
+            return BaseOutput.failure(e.getMessage());
+        } catch (Exception e) {
+            logger.error("quit", e);
+            return BaseOutput.failure(e.getMessage());
+        }
+    }
+
+    @ApiOperation(value = "查询下架商品列表", notes = "查询下架商品列表")
+    @RequestMapping(value = "/unavailableProduct.api", method = RequestMethod.POST)
+    @InterceptConfiguration
+    public BaseOutput<BasePage<TradeDetail>> unavailableProduct(@RequestBody TradeDetail tradeDetail) {
+        if (tradeDetail == null || null == tradeDetail.getBuyerId()) {
+            return BaseOutput.failure("参数错误");
+        }
+        try {
+
+            tradeDetail.setSort("product_name");
+            tradeDetail.setOrder("desc");
+            tradeDetail.setMetadata(IDTO.AND_CONDITION_EXPR, " pushaway_weight > 0");
+            return BaseOutput.success().setData(tradeDetailService.listPageByExample(tradeDetail));
+        } catch (TraceBusinessException e) {
+            return BaseOutput.failure(e.getMessage());
+        } catch (Exception e) {
+            logger.error("quit", e);
+            return BaseOutput.failure(e.getMessage());
+        }
     }
 }
