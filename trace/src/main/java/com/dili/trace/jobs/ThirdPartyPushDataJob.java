26eacd9b31c443830dc356bec76153a9eee6daaf
lily.huang <SIT_Lily>
2020-08-27 18:36:12 +0800

    接口

--- trace/src/main/java/com/dili/trace/jobs/ThirdPartyPushDataJob.java
+++ trace/src/main/java/com/dili/trace/jobs/ThirdPartyPushDataJob.java
@@ -14,6 +14,12 @@ import com.dili.trace.service.CategoryService;
 import com.dili.trace.service.DataReportService;
 import com.dili.trace.service.ImageCertService;
 import com.dili.trace.service.ThirdPartyPushDataService;
+import com.dili.trace.dto.ThirdPartyReportDataQueryDto;
+import com.dili.trace.dto.thirdparty.report.*;
+import com.dili.trace.enums.PreserveTypeEnum;
+import com.dili.trace.glossary.UserQrStatusEnum;
+import com.dili.trace.service.*;
+import com.google.common.collect.Lists;
 import one.util.streamex.StreamEx;
 import org.apache.commons.collections4.CollectionUtils;
 import org.slf4j.Logger;
@@ -25,6 +31,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
 
 @Component
@@ -47,8 +54,12 @@ public class ThirdPartyPushDataJob implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-         Optional<OperatorUser> optUser = Optional.of(new OperatorUser(-1L, "auto"));
-         this.reportRegisterBill(optUser);
+         //Optional<OperatorUser> optUser = Optional.of(new OperatorUser(-1L, "auto"));
+         //this.reportRegisterBill(optUser);
+        /*Optional<OperatorUser> optUser = Optional.of(new OperatorUser(-1L, "auto"));
+        this.pushBigCategory(optUser);
+        this.pushCategory("category_smallClass", "商品二级类目新增/修改", 1, optUser);
+        this.pushCategory("category_goods", "商品新增/修改", 2, optUser);*/
     }
 
     // 每五分钟提交一次数据
@@ -56,39 +67,100 @@ public class ThirdPartyPushDataJob implements CommandLineRunner {
     public void pushData() {
         Optional<OperatorUser> optUser = Optional.of(new OperatorUser(-1L, "auto"));
         try {
-            this.pushCategory(1);
+            this.pushBigCategory(optUser);
+            this.pushCategory("category_smallClass", "商品二级类目新增/修改", 1, optUser);
+            this.pushCategory("category_goods", "商品新增/修改", 2, optUser);
             this.reportRegisterBill(optUser);
         } catch (Exception e) {
             logger.error(e.getMessage(), e);
         }
     }
 
-    private void pushCategory(Integer level)
+    private void pushBigCategory(Optional<OperatorUser> optUser)
     {
-        String tableName = "category";
+        String tableName = "category_bigClass";
         String interfaceName = "商品大类新增/修改";
-        Category category = new Category();
-        category.setLevel(level);
-        List<Category> categories = categoryService.list(category);
-        List<ThirdPartyPushData> pushDataList = new ArrayList<>();
-        List<Category> pushCategories = StreamEx.of(categories).filter(td -> {
-            ThirdPartyPushData thirdPartyPushData =
-                    thirdPartyPushDataService.getThredPartyPushData(tableName, td.getId());
-            if (thirdPartyPushData == null || (thirdPartyPushData != null
-                    && thirdPartyPushData.getPushTime().compareTo(td.getModified()) < 0))
+        ThirdPartyPushData thirdPartyPushData =
+                thirdPartyPushDataService.getThredPartyPushData(tableName);
+        if(thirdPartyPushData == null) {
+            PreserveTypeEnum[] preserveTypeEnums = PreserveTypeEnum.values();
+            List<CategoryDto> categoryDtos = new ArrayList<>();
+            int i = 1;
+            for(PreserveTypeEnum td : preserveTypeEnums){
+                CategoryDto categoryDto = new CategoryDto();
+                categoryDto.setThirdBigClassName(td.getName());
+                categoryDto.setThirdBigClassId(String.valueOf(i));
+                categoryDtos.add(categoryDto);
+                i++;
+            };
+            BaseOutput baseOutput = this.dataReportService.reportCategory(categoryDtos, optUser);
+            if(baseOutput.isSuccess())
             {
                 ThirdPartyPushData pushData = new ThirdPartyPushData();
                 pushData.setTableName(tableName);
-//                pushData.setTableId(td.getId());
                 pushData.setInterfaceName(interfaceName);
-                pushDataList.add(pushData);
-                return true;
+                this.thirdPartyPushDataService.updatePushTime(pushData);
             }
-            return false;
-        }).toList();
+            else
+            {
+                logger.error("上报:商品大类新增/修改 失败，原因:{}", baseOutput.getMessage());
+            }
+        }
+    }
 
+    private void pushCategory(String tableName, String interfaceName,
+                              Integer level, Optional<OperatorUser> optUser)
+    {
+        ThirdPartyPushData thirdPartyPushData =
+                thirdPartyPushDataService.getThredPartyPushData(tableName);
+        Category category = new Category();
+        category.setLevel(level);
+        List<Category> categories = categoryService.list(category);
+        ThirdPartyPushData pushData = new ThirdPartyPushData();
+        pushData.setTableName(tableName);
+        pushData.setInterfaceName(interfaceName);
+        BaseOutput baseOutput = new BaseOutput();
+        if(tableName.equals("category_smallClass")) {
+            List<CategorySecondDto> categoryDtos = new ArrayList<>();
+            int i = 1;
+            PreserveTypeEnum[] preserveTypeEnums = PreserveTypeEnum.values();
+            for(PreserveTypeEnum type : preserveTypeEnums) {
+                for(Category td : categories){
+                    if (thirdPartyPushData == null || thirdPartyPushData.getPushTime().compareTo(td.getModified()) < 0) {
+                        CategorySecondDto categoryDto = new CategorySecondDto();
+                        categoryDto.setThirdSmallClassId(td.getId().toString());
+                        categoryDto.setSmallClassName(td.getName());
+                        categoryDto.setThirdBigClassId(String.valueOf(i));
+                        categoryDtos.add(categoryDto);
+                    }
+                };
+                i++;
+            }
+            baseOutput = this.dataReportService.reportSecondCategory(categoryDtos, optUser);
+        }
+        else if(tableName.equals("category_goods"))
+        {
+            List<GoodsDto> categoryDtos = new ArrayList<>();
+            StreamEx.of(categories).forEach(td -> {
+                if (thirdPartyPushData == null || thirdPartyPushData.getPushTime().compareTo(td.getModified()) < 0){
+                    GoodsDto categoryDto = new GoodsDto();
+                    categoryDto.setGoodsName(td.getName());
+                    categoryDto.setThirdGoodsId(td.getId().toString());
+                    categoryDto.setThirdSmallClassId(td.getParentId().toString());
+                    categoryDtos.add(categoryDto);
+                }
+            });
+            baseOutput = this.dataReportService.reportGoods(categoryDtos, optUser);
+        }
 
-        this.thirdPartyPushDataService.updatePushTime(pushDataList);
+        if(baseOutput.isSuccess()) {
+            this.thirdPartyPushDataService.updatePushTime(pushData);
+        }
+        else
+        {
+            logger.error("上报:{} 失败，原因:{}", interfaceName, baseOutput.getMessage());
+        }
+
     }
 
     public BaseOutput reportRegisterBill(Optional<OperatorUser> optUser) {
@@ -129,7 +201,7 @@ public class ThirdPartyPushDataJob implements CommandLineRunner {
         if (baseOutput.isSuccess()) {
             thirdPartyPushData = thirdPartyPushData == null ? new ThirdPartyPushData() : thirdPartyPushData;
             // TODO:endTime应该传入进去
-            this.thirdPartyPushDataService.updatePushTime(Arrays.asList(thirdPartyPushData));
+            this.thirdPartyPushDataService.updatePushTime(thirdPartyPushData);
         }
         return baseOutput;
     }
