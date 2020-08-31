8c6751645438a1e482e2c6f083542d5858c3c36a
lily.huang <SIT_Lily>
2020-08-31 09:02:42 +0800

    接口

--- doc/ddl/hz/建表third_party_push_data.sql
+++ doc/ddl/hz/建表third_party_push_data.sql
@@ -4,6 +4,6 @@ CREATE TABLE `third_party_push_data` (
   `interface_name` varchar(50) NOT NULL,
   `table_name` varchar(50) NOT NULL,
   `push_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `created` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
