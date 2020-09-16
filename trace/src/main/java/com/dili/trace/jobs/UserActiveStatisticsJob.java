82686dc215e9bd08c43dfe4055620a5229e656be
SIT_Joy <sit_joy@sit.com>
2020-09-16 11:00:03 +0800

    2

--- trace/src/main/java/com/dili/trace/jobs/UserActiveStatisticsJob.java
+++ trace/src/main/java/com/dili/trace/jobs/UserActiveStatisticsJob.java
@@ -27,7 +27,7 @@ public class UserActiveStatisticsJob implements CommandLineRunner {
     /**
      * 每天中午12点更新一次数据
      */
-    @Scheduled(cron = "0 0 12 * * ?")
+    @Scheduled(cron = "0 06 11 * * ?")
     public void pushData() {
         try {
             updateUserActive();
