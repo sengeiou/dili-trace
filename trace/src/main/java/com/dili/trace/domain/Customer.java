d6d38530adcd9e781c0b4cd9a806526c8d980dd6
wangguofeng <wangguofeng@diligr.com>
2020-11-21 11:07:58 +0800

    增加用户及卡信息接口

--- trace/src/main/java/com/dili/trace/domain/Customer.java
+++ trace/src/main/java/com/dili/trace/domain/Customer.java
@@ -1,9 +1,11 @@
 package com.dili.trace.domain;
 
+import com.dili.customer.sdk.domain.dto.CustomerExtendDto;
 import com.dili.ss.dto.IBaseDomain;
 import com.dili.ss.metadata.FieldEditor;
 import com.dili.ss.metadata.annotation.EditMode;
 import com.dili.ss.metadata.annotation.FieldDef;
+import com.dili.trace.util.MaskUserInfo;
 import io.swagger.annotations.ApiModelProperty;
 
 import javax.persistence.*;
@@ -14,62 +16,105 @@ import javax.persistence.Id;
 
 /**
  * 由MyBatis Generator工具自动生成
- * 
+ * <p>
  * This file was generated on 2019-07-31 14:56:14.
  */
-@Table(name = "`vCustomer`")
-public interface Customer extends IBaseDomain {
+public class Customer {
 
+    private Long id;
 
-    void setId(Long id);
     @ApiModelProperty(value = "交易账号")
-    @Column(name = "`customerID`")
-    @FieldDef(label="customerId")
-    @EditMode(editor = FieldEditor.Text, required = false)
-    String getCustomerId();
+    private String customerId;
 
-    void setCustomerId(String customerId);
     @ApiModelProperty(value = "客户姓名")
-    @Column(name = "`name`")
-    @FieldDef(label="name", maxLength = 100)
-    @EditMode(editor = FieldEditor.Text, required = false)
-    String getName();
+    private String name;
 
-    void setName(String name);
     @ApiModelProperty(value = "客户身份证")
-    @Column(name = "`IDNo`")
-    @FieldDef(label="idNo", maxLength = 20)
-    @EditMode(editor = FieldEditor.Text, required = false)
-    String getIdNo();
-
-    void setIdNo(String idNo);
+    private String idNo;
     @ApiModelProperty(value = "客户地址")
-    @Column(name = "`address`")
-    @FieldDef(label="address", maxLength = 50)
-    @EditMode(editor = FieldEditor.Text, required = false)
-    String getAddress();
-
-    void setAddress(String address);
+    private String address;
 
     @ApiModelProperty(value = "业户手机号")
-    @Column(name = "`phone`")
-    @EditMode(editor = FieldEditor.Text, required = false)
-    String getPhone();
+    private String phone;
 
-    void setPhone(String phone);
+    @ApiModelProperty(value = "印刷卡号(客户账号)")
+    private String printingCard;
 
-    @ApiModelProperty(value = "印刷卡号")
-    @Column(name = "`printingCode`")
-    @FieldDef(label="printingCard", maxLength = 50)
-    @EditMode(editor = FieldEditor.Text, required = false)
-    String getPrintingCard();
 
-    void setPrintingCard(String printingCard);
+    @ApiModelProperty(value = "作废状态")
+//    @FieldDef(label="0:作废，1：活跃")
+    private Integer active;
+    public static Customer convert(CustomerExtendDto dto){
+        Customer c = new Customer();
+        c.setAddress(dto.getCertificateAddr());
+        c.setCustomerId(String.valueOf(dto.getId()));
+        c.setIdNo(dto.getCertificateNumber());
+        c.setName(dto.getName());
+        c.setPhone(dto.getContactsPhone());
+        return c;
 
-    @Column(name = "`active`")
-    @FieldDef(label="0:作废，1：活跃")
-    @EditMode(editor = FieldEditor.Number, required = false)
-    Integer getActive();
+    }
+    public Long getId() {
+        return id;
+    }
 
-    void setActive(Integer active);
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIdNo() {
+        return idNo;
+    }
+
+    public void setIdNo(String idNo) {
+        this.idNo = idNo;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPrintingCard() {
+        return printingCard;
+    }
+
+    public void setPrintingCard(String printingCard) {
+        this.printingCard = printingCard;
+    }
+
+    public Integer getActive() {
+        return active;
+    }
+
+    public void setActive(Integer active) {
+        this.active = active;
+    }
 }
