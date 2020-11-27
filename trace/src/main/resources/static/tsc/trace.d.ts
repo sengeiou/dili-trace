0f5e9c59ea1646a40924c2716ab1d3efa0bac1e8
wangguofeng <wangguofeng@diligr.com>
2020-11-27 12:03:28 +0800

    寿光列表页面可用

---
+++ trace/src/main/resources/static/tsc/trace.d.ts
@@ -0,0 +1,36 @@
+/* tslint:disable */
+/* eslint-disable */
+// Generated using typescript-generator version 2.27.744 on 2020-11-27 11:44:26.
+
+interface Market {
+    id: number;
+    code: string;
+    name: string;
+    appId: number;
+    appSecret: string;
+    contextUrl: string;
+    platformMarketId: number;
+}
+
+declare const enum RegisterBillStateEnum {
+    NEW = 0,
+    WAIT_AUDIT = 1,
+    WAIT_SAMPLE = 2,
+    WAIT_CHECK = 4,
+    CHECKING = 5,
+    ALREADY_CHECK = 6,
+    ALREADY_AUDIT = 7,
+}
+
+declare const enum BillDetectStateEnum {
+    PASS = "PASS",
+    NO_PASS = "NO_PASS",
+    REVIEW_PASS = "REVIEW_PASS",
+    REVIEW_NO_PASS = "REVIEW_NO_PASS",
+}
+
+declare const enum RegisterSourceEnum {
+    OTHERS = 0,
+    TALLY_AREA = 1,
+    TRADE_AREA = 2,
+}
