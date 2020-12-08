91404c24bcb86c560b8cb91d11f238645d7a5852
Alvin.Li <alvin.li@skywares.com>
2020-12-08 14:00:32 +0800

    无用导包删除

--- trace/src/main/java/com/dili/trace/controller/DetectRequestController.java
+++ trace/src/main/java/com/dili/trace/controller/DetectRequestController.java
@@ -1,14 +1,8 @@
 package com.dili.trace.controller;
 
 import com.dili.ss.domain.EasyuiPageOutput;
-import com.dili.ss.dto.DTOUtils;
-import com.dili.ss.util.DateUtils;
-import com.dili.trace.dto.BillReportQueryDto;
 import com.dili.trace.dto.DetectRequestDto;
-import com.dili.trace.dto.RegisterBillDto;
-import com.dili.trace.service.*;
-import com.dili.trace.util.BeanMapUtil;
-import com.dili.uap.sdk.domain.UserTicket;
+import com.dili.trace.service.DetectRequestService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
@@ -18,7 +12,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -26,7 +19,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
 
 /**
  * 检测请求业务类
@@ -36,37 +28,6 @@ import java.util.Date;
 @RequestMapping("/detectRequest")
 public class DetectRequestController {
     private static final Logger logger = LoggerFactory.getLogger(DetectRequestController.class);
-
-    @Autowired
-    BillService billService;
-
-    @Autowired
-    SgRegisterBillService registerBillService;
-    @Autowired
-    TradeTypeService tradeTypeService;
-
-    @Autowired
-    CustomerService customerService;
-    @Autowired
-    DetectRecordService detectRecordService;
-
-    @Autowired
-    SeparateSalesRecordService separateSalesRecordService;
-    @Autowired
-    UserService userService;
-    @Autowired
-    UserPlateService userPlateService;
-    @Autowired
-    UsualAddressService usualAddressService;
-    @Autowired
-    UpStreamService upStreamService;
-    @Autowired
-    ImageCertService imageCertService;
-    @Autowired
-    UapRpcService uapRpcService;
-
-    @Autowired
-    QualityTraceTradeBillService qualityTraceTradeBillService;
 
     @Autowired
     DetectRequestService detectRequestService;
@@ -99,7 +60,6 @@ public class DetectRequestController {
     @RequestMapping(value = "/listPage.action", method = {RequestMethod.GET, RequestMethod.POST})
     public @ResponseBody String listPage(DetectRequestDto detectRequestDto) throws Exception {
         EasyuiPageOutput out=this.detectRequestService.listEasyuiPageByExample(detectRequestDto);
-        // EasyuiPageOutput out=this.detectRequestService.listEasyuiPageByExample(detectRequestDto,true);
         return out.toString();
     }
 
