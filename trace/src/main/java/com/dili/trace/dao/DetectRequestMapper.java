ff1cee0467c07318442cdd4242b9481fccea33dc
Alvin.Li <alvin.li@skywares.com>
2020-12-12 15:53:15 +0800

    app端采样检测模块接口

--- trace/src/main/java/com/dili/trace/dao/DetectRequestMapper.java
+++ trace/src/main/java/com/dili/trace/dao/DetectRequestMapper.java
@@ -1,7 +1,10 @@
 package com.dili.trace.dao;
 
 import com.dili.ss.base.MyMapper;
+import com.dili.trace.api.input.DetectRequestQueryDto;
+import com.dili.trace.api.output.SampleSourceCountOutputDto;
 import com.dili.trace.domain.DetectRequest;
+import com.dili.trace.dto.DetectRequestDto;
 import com.dili.trace.enums.DetectStatusEnum;
 import org.apache.ibatis.annotations.Param;
 
