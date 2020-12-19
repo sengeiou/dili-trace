dd105f05479e032590158c03ee691dc3c2e69e63
wangguofeng <wangguofeng@diligr.com>
2020-12-19 14:51:20 +0800

    增加微服务接口

--- trace/src/main/java/com/dili/TraceWebApplication.java
+++ trace/src/main/java/com/dili/TraceWebApplication.java
@@ -30,14 +30,14 @@ import tk.mybatis.spring.annotation.MapperScan;
 //处理事务支持
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 @MapperScan(basePackages = {"com.dili.trace.dao", "com.dili.ss.dao", "com.dili.ss.uid.dao"})
-@ComponentScan(basePackages = {"com.dili.ss", "com.dili.trace", "com.dili.common", "com.dili.commons", "com.dili.uap.sdk"})
-@RestfulScan({"com.dili.trace.rpc", "com.dili.uap.sdk.rpc", "com.dili.bpmc.sdk.rpc"})
+@ComponentScan(basePackages = {"com.dili.ss.uid","com.dili.ss", "com.dili.trace", "com.dili.common", "com.dili.commons", "com.dili.uap.sdk"})
+@RestfulScan({"com.dili.ss.uid","com.dili.trace.rpc", "com.dili.uap.sdk.rpc", "com.dili.bpmc.sdk.rpc"})
 //@DTOScan({"com.dili.trace","com.dili.ss"})
 //@Import(DynamicRoutingDataSourceRegister.class)
 @EnableScheduling
 
 @EnableAsync
-@EnableFeignClients(basePackages = {"com.dili.assets.sdk.rpc"
+@EnableFeignClients(basePackages = {"com.dili.ss.uid","com.dili.assets.sdk.rpc"
         , "com.dili.customer.sdk.rpc"
         , "com.dili.trace.rpc"
         , "com.dili.bpmc.sdk.rpc"})
