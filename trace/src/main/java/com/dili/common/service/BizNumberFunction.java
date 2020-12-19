95132b9cfea701a1e36c4dbd1e09057cef5e2e68
wangguofeng <wangguofeng@diligr.com>
2020-12-19 14:51:47 +0800

    fix bug

--- trace/src/main/java/com/dili/common/service/BizNumberFunction.java
+++ trace/src/main/java/com/dili/common/service/BizNumberFunction.java
@@ -18,7 +18,7 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class BizNumberFunction {
-    @Autowired
+    @Autowired(required = false)
     private BizNumberService bizNumberService;
 
     /**
