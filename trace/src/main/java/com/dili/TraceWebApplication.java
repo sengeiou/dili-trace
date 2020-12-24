1a00656a88ff60653a8deb7cc7823cd93742c338
wangguofeng <wangguofeng@diligr.com>
2020-12-24 15:17:49 +0800

    fix bug

--- trace/src/main/java/com/dili/TraceWebApplication.java
+++ trace/src/main/java/com/dili/TraceWebApplication.java
@@ -29,7 +29,7 @@ import tk.mybatis.spring.annotation.MapperScan;
 @SpringBootApplication
 //处理事务支持
 @EnableAspectJAutoProxy(proxyTargetClass = true)
-@MapperScan(basePackages = {"com.dili.trace.dao", "com.dili.ss.dao", "com.dili.ss.uid.dao"})
+@MapperScan(basePackages = {"com.dili.ss.uid.mapper","com.dili.trace.dao", "com.dili.ss.dao"})
 @ComponentScan(basePackages = {"com.dili.ss.uid","com.dili.ss", "com.dili.trace", "com.dili.common", "com.dili.commons", "com.dili.uap.sdk"})
 @RestfulScan({"com.dili.ss.uid","com.dili.trace.rpc", "com.dili.uap.sdk.rpc", "com.dili.bpmc.sdk.rpc"})
 //@DTOScan({"com.dili.trace","com.dili.ss"})
