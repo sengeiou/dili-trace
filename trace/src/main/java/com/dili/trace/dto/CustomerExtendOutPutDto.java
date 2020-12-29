04b1037d94d4fc58feaf4405b0c6f078fc673000
Lily.Huang <lily.huang@skywares.com>
2020-12-29 15:02:19 +0800

    用户管理返回数据新增市场id和市场名称

---
+++ trace/src/main/java/com/dili/trace/dto/CustomerExtendOutPutDto.java
@@ -0,0 +1,37 @@
+package com.dili.trace.dto;
+
+import com.dili.customer.sdk.domain.TallyingArea;
+import com.dili.customer.sdk.domain.dto.CharacterTypeGroupDto;
+import com.dili.customer.sdk.domain.dto.CustomerExtendDto;
+
+import java.util.List;
+
+/**
+ * Description:
+ *
+ * @author Lily.Huang
+ * @date 2020/12/29
+ */
+public class CustomerExtendOutPutDto extends CustomerExtendDto {
+    private Long marketId;
+    private String marketName;
+
+    public Long getMarketId() {
+        return marketId;
+    }
+
+    public void setMarketId(Long marketId) {
+        this.marketId = marketId;
+    }
+
+    public CustomerExtendOutPutDto() {
+    }
+
+    public String getMarketName() {
+        return marketName;
+    }
+
+    public void setMarketName(String marketName) {
+        this.marketName = marketName;
+    }
+}
