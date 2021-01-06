9d7b3331ab340d61e71d2cae6561bef1a35ce707
wangguofeng <wangguofeng@diligr.com>
2021-01-06 13:57:21 +0800

    去掉多余代码

--- trace/src/main/java/com/dili/trace/advice/ControllerExceptionHandler.java
+++ trace/src/main/java/com/dili/trace/advice/ControllerExceptionHandler.java
@@ -1,9 +1,11 @@
 package com.dili.trace.advice;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -16,14 +18,19 @@ public class ControllerExceptionHandler {
 
     /**
      * 处理exception
-     *
      * @param request
+     * @param response
+     * @param handler
      * @param ex
      * @return
      * @throws Throwable
      */
-    @ExceptionHandler()
-    public Object handleServiceMessageException(HttpServletRequest request, Throwable ex) throws Throwable {
+    @ExceptionHandler(Exception.class)
+    public Object handleServiceMessageException(
+            HttpServletRequest request, HttpServletResponse response, @Nullable Object handler, Exception ex) throws Throwable {
+        if (handler != null) {
+            logger.info("handler.class={}", handler.getClass());
+        }
         logger.error(ex.getMessage(), ex);
         throw ex;
     }
