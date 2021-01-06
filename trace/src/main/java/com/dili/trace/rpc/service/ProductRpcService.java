f64d97509ea7b5bd1a23ce51aa82df36690974e8
wangguofeng <wangguofeng@diligr.com>
2021-01-06 11:19:22 +0800

    fix bug

--- trace/src/main/java/com/dili/trace/rpc/service/ProductRpcService.java
+++ trace/src/main/java/com/dili/trace/rpc/service/ProductRpcService.java
@@ -279,6 +279,7 @@ public class ProductRpcService {
 
         obj.setRegDetailDtos(Lists.newArrayList(detailDto));
         obj.setSource(StockRegisterSourceEnum.REG.getCode());
+        obj.setInStockNo(String.valueOf(bill.getBillId()));
         return obj;
     }
 }
