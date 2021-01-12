be9052fcc5819f4d286d692f9807a113e45b9bac
Asa <asa.lee@skywares.com>
2021-01-12 15:12:20 +0800

    PC-卖家报备：优化查询字段名称

--- trace/src/main/java/com/dili/trace/domain/RegisterBill.java
+++ trace/src/main/java/com/dili/trace/domain/RegisterBill.java
@@ -595,7 +595,7 @@ public class RegisterBill extends BaseDomain {
     private String isPrintCheckSheetName;
 
     public String getIsPrintCheckSheetName() {
-        return YesOrNoEnum.getYesOrNoEnum(this.getIsPrintCheckSheet()).getName();
+        return this.getIsPrintCheckSheet() == null ? null : YesOrNoEnum.getYesOrNoEnum(this.getIsPrintCheckSheet()).getName();
     }
 
     public void setIsPrintCheckSheetName(String isPrintCheckSheetName) {
@@ -684,7 +684,7 @@ public class RegisterBill extends BaseDomain {
     private String corporateName;
 
     /**
-     *商品别名
+     * 商品别名
      */
     @ApiModelProperty(value = "商品别名")
     @Column(name = "`product_alias_name`")
@@ -1395,7 +1395,7 @@ public class RegisterBill extends BaseDomain {
 
     @Transient
     public String getSourceDesc() {
-         return this.getRegisterSourceName() + (StringUtils.isBlank(this.sourceName) ? "" : ("：" + this.sourceName));
+        return this.getRegisterSourceName() + (StringUtils.isBlank(this.sourceName) ? "" : ("：" + this.sourceName));
     }
 
     public void setRegisterSource(Integer registerSource) {
