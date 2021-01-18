3319b74e38f780fe783fe2ff61741fe8925eeec7
Ron.Peng <lily.huang@skywares.com>
2021-01-18 14:08:34 +0800

    fix bug923 新增三个搜索项：手机号码，报备编号，进门时间

--- trace/src/main/java/com/dili/trace/dto/query/TruckEnterRecordQueryDto.java
+++ trace/src/main/java/com/dili/trace/dto/query/TruckEnterRecordQueryDto.java
@@ -1,6 +1,7 @@
 package com.dili.trace.dto.query;
 
 import com.dili.ss.domain.annotation.Like;
+import com.dili.ss.domain.annotation.Operator;
 import com.dili.trace.domain.TruckEnterRecord;
 import io.swagger.annotations.ApiModelProperty;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -8,21 +9,49 @@ import org.springframework.format.annotation.DateTimeFormat;
 import javax.persistence.Column;
 import javax.persistence.Transient;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 
 public class TruckEnterRecordQueryDto extends TruckEnterRecord {
-    // 登记时间开始
     /**
-     * 开始时间
+     * 登记时间开始
      */
-    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE, pattern = "yyyy-MM-dd")
-    private LocalDate createdStart;
-    // 登记时间结束
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME, pattern = "yyyy-MM-dd HH:mm:ss")
+    @Column(name = "`created`")
+    @Operator(Operator.GREAT_EQUAL_THAN)
+    private LocalDateTime createdStart;
+
     /**
-     * 结束时间
+     * 登记时间结束
      */
-    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE, pattern = "yyyy-MM-dd")
-    private LocalDate createdEnd;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME, pattern = "yyyy-MM-dd HH:mm:ss")
+    @Column(name = "`created`")
+    @Operator(Operator.LITTLE_EQUAL_THAN)
+    private LocalDateTime createdEnd;
+
+    /**
+     * 进门时间开始
+     */
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME, pattern = "yyyy-MM-dd HH:mm:ss")
+    @Column(name = "`created`")
+    @Operator(Operator.GREAT_EQUAL_THAN)
+    private LocalDateTime checkinCreatedStart;
+
+    /**
+     * 进门时间结束
+     */
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME, pattern = "yyyy-MM-dd HH:mm:ss")
+    @Column(name = "`created`")
+    @Operator(Operator.LITTLE_EQUAL_THAN)
+    private LocalDateTime checkinCreatedEnd;
+
+    /**
+     * 车牌模糊查询
+     */
+    @Column(name = "`code`")
+    @Like(Like.RIGHT)
+    private String likeCode;
+
 
     /**
      * 车牌模糊查询
@@ -30,6 +59,14 @@ public class TruckEnterRecordQueryDto extends TruckEnterRecord {
     @Column(name = "`truck_plate`")
     @Like(Like.RIGHT)
     private String likeTruckPlate;
+
+    public String getLikeCode() {
+        return likeCode;
+    }
+
+    public void setLikeCode(String likeCode) {
+        this.likeCode = likeCode;
+    }
 
     /**
      * 司机名字模糊查询
@@ -68,20 +105,35 @@ public class TruckEnterRecordQueryDto extends TruckEnterRecord {
         this.likeDriverName = likeDriverName;
     }
 
-    public LocalDate getCreatedStart() {
+    public LocalDateTime getCreatedStart() {
         return createdStart;
     }
 
-    public void setCreatedStart(LocalDate createdStart) {
+    public void setCreatedStart(LocalDateTime createdStart) {
         this.createdStart = createdStart;
     }
 
-    public LocalDate getCreatedEnd() {
+    public LocalDateTime getCreatedEnd() {
         return createdEnd;
     }
 
-    public void setCreatedEnd(LocalDate createdEnd) {
+    public void setCreatedEnd(LocalDateTime createdEnd) {
         this.createdEnd = createdEnd;
+    }
+    public LocalDateTime getCheckinCreatedStart() {
+        return checkinCreatedStart;
+    }
+
+    public void setCheckinCreatedStart(LocalDateTime checkinCreatedStart) {
+        this.checkinCreatedStart = checkinCreatedStart;
+    }
+
+    public LocalDateTime getCheckinCreatedEnd() {
+        return checkinCreatedEnd;
+    }
+
+    public void setCheckinCreatedEnd(LocalDateTime checkinCreatedEnd) {
+        this.checkinCreatedEnd = checkinCreatedEnd;
     }
 
     public String getLikeTruckPlate() {
