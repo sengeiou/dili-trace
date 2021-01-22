7007e1f411d68c45b4f624520758c0133af8c036
wangguofeng <wangguofeng@diligr.com>
2021-01-22 09:41:28 +0800

    fix bug-4

--- doc/ddl/2021-01-18.wgf.sql
+++ doc/ddl/2021-01-18.wgf.sql
@@ -3,4 +3,7 @@ ALTER TABLE dili_trace.user_store ADD market_name varchar(100) NULL COMMENT '市
 ALTER TABLE dili_trace.detect_record MODIFY COLUMN pd_result varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '检测项的结果';
 ALTER TABLE dili_trace.register_bill ADD return_reason varchar(100) NULL COMMENT '检测退回原因';
 ALTER TABLE dili_trace.trade_detail ADD buyer_type INT DEFAULT 1 NOT NULL COMMENT '买家类型';
+ALTER TABLE dili_trace.trade_request MODIFY COLUMN buyer_id bigint NULL COMMENT '买家ID';
+ALTER TABLE dili_trace.trade_order MODIFY COLUMN buyer_id bigint NULL COMMENT '买家ID';
+
 
