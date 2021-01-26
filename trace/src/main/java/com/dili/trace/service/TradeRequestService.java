4eec008eb6db217b0133e630dcc51903ab7efbc2
wangguofeng <wangguofeng@diligr.com>
2021-01-26 14:18:48 +0800

    fix bug[CG-1449]

--- trace/src/main/java/com/dili/trace/service/TradeRequestService.java
+++ trace/src/main/java/com/dili/trace/service/TradeRequestService.java
@@ -886,12 +886,12 @@ public class TradeRequestService extends BaseServiceImpl<TradeRequest, Long> {
 
                 });
 
-                UserStore userStore = new UserStore();
-                userStore.setUserId(sellerId);
-                UserStore userStoreExists = StreamEx.of(userStoreService.list(userStore)).nonNull().findFirst().orElse(null);
-                if (userStoreExists != null && StringUtils.isNoneBlank(userStoreExists.getStoreName())) {
-                    outPutDto.setUserName(userStoreExists.getStoreName());
-                }
+//                UserStore userStore = new UserStore();
+//                userStore.setUserId(sellerId);
+//                UserStore userStoreExists = StreamEx.of(userStoreService.list(userStore)).nonNull().findFirst().orElse(null);
+//                if (userStoreExists != null && StringUtils.isNoneBlank(userStoreExists.getStoreName())) {
+//                    outPutDto.setUserName(userStoreExists.getStoreName());
+//                }
                 return outPutDto;
 
             }).orElse(null);
