b48206b6adc000eda9a7a1779d8f1544922a1d4c
Brain <Brain.Wang@skywares.com>
2021-01-27 09:47:21 +0800

    抽检

--- trace/src/main/resources/static/tsc/CustomerDetectRequestGrid.index.ts
+++ trace/src/main/resources/static/tsc/CustomerDetectRequestGrid.index.ts
@@ -25,6 +25,8 @@ class CustomerDetectRequestGrid extends ListPage {
         $('#detail-btn').on('click',async ()=>await this.openDetailPage());
         $('#createSheet-btn').on('click',async ()=>await this.openCreateSheetPage());
         $('#upload-handleresult-btn').on('click',async ()=>await this.openUploadHandleResultPage());
+        $('#sample-btn').on('click',async ()=>await this.openSpotCheck());
+        $('#unqualified-handle-btn').on('click',async ()=>await this.openSpotHandle());
         $('select[name="detectResultSelect"]').on('change',async (o,n)=>{
             var data=JSON.parse($('select[name="detectResultSelect"]').val().toString());
             $('input[name="detectType"]').val(data['detectType']);
@@ -33,7 +35,43 @@ class CustomerDetectRequestGrid extends ListPage {
         this.grid.on('check.bs.table uncheck.bs.table', async () => await this.resetButtons());
     }
 
+    /**
+     * 上传抽检处理结果
+     */
+    private async openSpotHandle(){
+        var row=this.rows[0]
+        var url=this.toUrl('/customerDetectRequest/unqualifiedHandle.html?id='+row.billId);
+        //@ts-ignore
+        var spotDia = bs4pop.dialog({
+            title: '不合格处置',
+            content: url,
+            isIframe: true,
+            closeBtn: true,
+            backdrop: 'static',
+            width: '48%',
+            height: '78%',
+            btns: []
+        });
+    }
 
+    /**
+     * 打开抽检
+     */
+    private async openSpotCheck(){
+        var row=this.rows[0]
+        var url=this.toUrl('/customerDetectRequest/spotCheck.html?billId='+row.billId);
+        //@ts-ignore
+        var spotDia = bs4pop.dialog({
+            title: '抽检',
+            content: url,
+            isIframe: true,
+            closeBtn: true,
+            backdrop: 'static',
+            width: '98%',
+            height: '98%',
+            btns: []
+        });
+    }
     /**
      * 上传处理结果
      */
