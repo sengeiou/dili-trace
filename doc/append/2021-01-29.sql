433ad926b542111aa655da0b11f4f54b0e89c9e3
wangguofeng <wangguofeng@diligr.com>
2021-01-29 14:42:46 +0800

    fix bug

--- doc/append/2021-01-29.sql
+++ doc/append/2021-01-29.sql
@@ -1,1 +1,3 @@
 ALTER TABLE dili_trace.trade_detail MODIFY COLUMN buyer_id bigint NULL COMMENT '买家ID';
+ALTER TABLE dili_trace.register_head MODIFY COLUMN remain_weight decimal(14,3) DEFAULT 0.000 NOT NULL COMMENT '剩余重量';
+ALTER TABLE dili_trace.register_head MODIFY COLUMN piece_weight decimal(14,3) NULL COMMENT '件重';
