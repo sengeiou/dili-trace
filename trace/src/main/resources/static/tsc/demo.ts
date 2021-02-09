7433fe86945f1639d45598531fe990d05fb567f5
wangguofeng <wangguofeng@diligr.com>
2021-02-09 15:15:23 +0800

    add field config code

--- trace/src/main/resources/static/tsc/demo.ts
+++ trace/src/main/resources/static/tsc/demo.ts
@@ -8,17 +8,31 @@ export class DemoClass {
     }
 }
 
+export namespace abc {
+    export namespace def {
+        export class A {
+            constructor() {
+            }
+            hello: ()=>{};
+        };
+
+        export class B {
+        };
+    }
+}
 export namespace demo {
-
-
     export class Test1 {
         constructor() {
+            let a: abc.def.A = new abc.def.A();
+            (async ()=>{
+               await a.hello();
+            })();
         }
     }
-
     export class Test2 {
         constructor() {
         }
     }
 
 }
+console.info("abcd")
