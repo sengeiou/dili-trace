a56fa0087103d9e09e0f6deb846f928d846afeb8
wangguofeng <wangguofeng@diligr.com>
2021-03-02 17:35:58 +0800

    修改页面

--- trace/src/main/resources/static/tsc/trace.d.ts
+++ trace/src/main/resources/static/tsc/trace.d.ts
@@ -1,6 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
-// Generated using typescript-generator version 2.27.744 on 2021-03-02 10:20:11.
+// Generated using typescript-generator version 2.27.744 on 2021-03-02 14:25:32.
 
 interface Market {
     id: number;
