c02d25adc29f2ae9ad15637d8e7f94cb3f2aa154
wangguofeng <wangguofeng@diligr.com>
2021-03-25 15:24:29 +0800

    fix bug[更新版本]

--- trace/src/main/resources/static/tsc/trace.d.ts
+++ trace/src/main/resources/static/tsc/trace.d.ts
@@ -1,6 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
-// Generated using typescript-generator version 2.27.744 on 2021-03-24 15:27:57.
+// Generated using typescript-generator version 2.27.744 on 2021-03-25 15:23:37.
 
 interface Market {
     id: number;
