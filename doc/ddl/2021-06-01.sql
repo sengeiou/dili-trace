aff0dcc65896a50e69f3a6e5924266cc27ecdd15
wangguofeng <wangguofeng@diligr.com>
2021-06-05 11:20:56 +0800

    remove useless code

--- doc/ddl/2021-06-01.sql
+++ doc/ddl/2021-06-01.sql
@@ -1,3 +1,4 @@
 ALTER TABLE dili_trace.`product_stock` ADD soft_weight decimal(17, 3) NOT NULL DEFAULT 0.000 COMMENT '锁定库存重量';
 
 drop  table dili_trace.`message_config`;
+drop  table dili_trace.`applets_config`;
